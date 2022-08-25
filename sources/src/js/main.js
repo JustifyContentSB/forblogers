6debb6cacdc5ea580e71ad3e3bd32dbdf89d850c
RxPro <wxweb@mail.ru>
2022-08-25 21:14:22 +0300

    Верстка 3 страниц с адаптивом. В дизайне есть недочеты. Будут правки после согласования с дизайнером

---
+++ sources/src/js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+$(document).ready(function () {
+   // Your code here
+   pictureSlider()
+   inputWidth()
+});
+
+// Your functions here
+
+function pictureSlider() {
+   var swiper = new Swiper(".picture__slider", {
+      clickable: true,
+      pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+      },
+   });
+}
+
+// function inputWidth() {
+//    let elements = document.querySelectorAll('.field');
+
+//    let inputs = [];
+
+//    elements.forEach(function (element, index) {
+//       inputs[index] = (element, elements.setAttribute('size', elements.getAttribute('placeholder').length););
+//    })
+// }
+
+function inputWidth() {
+   let elements = document.querySelectorAll('.field');
+
+   elements.forEach(function (element, index) {
+      element.style.width = element.getAttribute('placeholder').length + 'ch';
+   });
+}
